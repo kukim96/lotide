fb6a67724f2bbe7c6da67d16fc9613900ad18465
Kiup Kim <kukim96@gmail.com>
2021-03-29 15:55:36 -0600

    implemented tail

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let tailArray = [];
+  for (let i = 1; i < array.length; i++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
+};
+
+const words = ["Hello", "Lighthouse", "Labs"];
+
+console.log(tail(words));
+assertEqual(words.length, 3);
+
+module.exports = tail;
