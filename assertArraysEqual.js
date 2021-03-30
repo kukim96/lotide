d328d18a79c0860aa7564768d22074109cc14f22
Kiup Kim <kukim96@gmail.com>
2021-03-30 16:48:48 -0600

    implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(array1, array2) {
+  const result = eqArrays(array1,array2);
+  if (result) {
+    console.log(`✅✅✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑   Assertion failed: ${array1} !== ${array2}`);
+  }
+};
