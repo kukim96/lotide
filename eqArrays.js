48b9217220c3be9f55eea4a69d4f0a483f56dd00
Kiup Kim <kukim96@gmail.com>
2021-03-30 23:03:19 -0600

    refactored codes

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   let output = true;
   if (array1.length !== array2.length) return false;
@@ -19,7 +11,3 @@ const eqArrays = function(array1, array2) {
   return output;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"],["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"],["1", "2", 3]), true);
