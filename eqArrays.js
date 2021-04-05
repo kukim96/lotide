43148bec745d8a093b9f29105cd8944665d9866f
Kiup Kim <kukim96@gmail.com>
2021-04-05 14:30:16 -0600

    move test codes

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,17 @@
-const eqArrays = function(array1, array2) {
+const assertEqual = require("./assertEqual");
+
+const eqArrays = function(actual, expected) {
   let output = true;
-  if (array1.length !== array2.length) return false;
-  for (let element = 0; element < array1.length; element += 1) {
-    if (Array.isArray(array1[element]) || Array.isArray(array2[element])) {
-      output = output && eqArrays(array1[element], array2[element]);
-    } else if (array1[element] !== array2[element]) {
-      output = output && false;
+  if (actual.length !== expected.length) {
+    output = false;
+  }  
+  for (let element = 0; element < actual.length; element ++) {
+    if (actual[element] !== expected[element]) {
     }
   }
   return output;
 };
 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+module.exports = eqArrays;
