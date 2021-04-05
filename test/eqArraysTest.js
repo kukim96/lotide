ff7f76cd6a5cd83c1bc455dc1278e62166dbbf6b
Kiup Kim <kukim96@gmail.com>
2021-04-05 15:26:17 -0600

    refactored to chai testing

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,17 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
 const actual = eqArrays([1, 2, 3]);
 const expected = [1, 2, 3]
 
-assertEqual(actual, expected, true);
+describe("#eqArrays", () => {
+  it("actual is equal to expected", () => {
+    const actual = eqArrays([1, 2, 3]);
+    const expected = [1, 2, 3]
+    assert.strictEqual(actual, expected, true);
+  })
+})
+
+//assertEqual(actual, expected, true);
 
