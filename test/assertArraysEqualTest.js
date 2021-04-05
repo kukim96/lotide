b75cd6aa9d876f52a6b5a6ad9aeaa3592f23c137
Kiup Kim <kukim96@gmail.com>
2021-04-05 15:25:31 -0600

    refactored to chai testing

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,4 +1,12 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(["I", "Love", "Coding"], ["I", "Love", "Coding"]);
-assertArraysEqual(["I", "Love", "Soccer"], ["I", "Love", "Baseball"]);
+//assertArraysEqual(["I", "Love", "Coding"], ["I", "Love", "Coding"]);
+//assertArraysEqual(["I", "Love", "Soccer"], ["I", "Love", "Baseball"]);
+
+describe("#assertArraysEqual", () => {
+  it("arry 1 = array 2", () => {
+    assert.strictEqual(assertArraysEqual(["I", "Love", "Coding"], ["I", "Love", "Coding"]))
+    assert.strictEqual(assertArraysEqual(["I", "Love", "Soccer"], ["I", "Love", "Baseball"]));
+  });
+});
