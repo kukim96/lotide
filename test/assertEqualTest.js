e1f1ff4a8def54e3e55d01a758edf6afcb90d7af
Kiup Kim <kukim96@gmail.com>
2021-04-05 15:25:56 -0600

    refactored to chai testing

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,20 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 3);
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+//assertEqual(2, 3);
+
+describe("#assertEqual", () => {
+  it("Lighthouse Labs is not equal toBootcamp", () => {
+    assert.strictEqual(assertEqual("Lighthouse Labs", "Bootcamp"));
+  });
+
+  it("1 is equal to 1", () => {
+    assert.strictEqual(assertEqual(1, 1));
+  });
+
+  it("2 is not equal to 3", () => {
+    assert.strictEqual(assertEqual(2, 3));
+  });
+});
