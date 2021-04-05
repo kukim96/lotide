a4860ee8610c9d59fd256d0818e82bdf4eb58c94
Kiup Kim <kukim96@gmail.com>
2021-04-05 14:45:47 -0600

    remove test codes

--- middle.js
+++ middle.js
@@ -19,9 +19,12 @@ const middle = function(arr1) {
   }
 };
 
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+(middle([1]));
+(middle([1, 2]));
+(middle([1, 2, 3]));
+(middle([1, 2, 3, 4, 5]));
+(middle([1, 2, 3, 4]));
+(middle([1, 2, 3, 4, 5, 6]));
+
+
+module.exports = middle;
