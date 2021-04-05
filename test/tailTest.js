ad79f9c7c9819f8aa977e310ac7a401968f2bd77
Kiup Kim <kukim96@gmail.com>
2021-04-05 15:28:02 -0600

    refactored to chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const words = ["Hello", "Lighthouse", "Labs"];
 
-tail(words);
+//tail(words);
+
+describe("#tail", () => {
+  it(undefined, () => {
+    const words = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(words));
+  })
+})
