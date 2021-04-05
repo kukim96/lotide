5476225d9f31bf5284ff81eaf4214eaf49efa859
Kiup Kim <kukim96@gmail.com>
2021-04-05 15:27:39 -0600

    refactored to chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,16 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]));
-assertArraysEqual(middle([1, 2]));
-assertArraysEqual(middle([1, 2, 3]));
-assertArraysEqual(middle([1, 2, 3, 4, 5]));
-assertArraysEqual(middle([1, 2, 3, 4]));
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]));
+//assertArraysEqual(middle([1]));
+//assertArraysEqual(middle([1, 2]));
+//assertArraysEqual(middle([1, 2, 3]));
+//assertArraysEqual(middle([1, 2, 3, 4, 5]));
+//assertArraysEqual(middle([1, 2, 3, 4]));
+//assertArraysEqual(middle([1, 2, 3, 4, 5, 6]));
+
+describe("#middle", () => {
+  it("middle of array consisting '1,2,3,4,5,6' is '3' and '4'", () => {
+    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4, 5, 6])));
+  });
+});
