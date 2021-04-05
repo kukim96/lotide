a000979dc27e6f56891b736079cec52b2fa23bc3
Kiup Kim <kukim96@gmail.com>
2021-04-05 15:27:16 -0600

    refactoed to chai testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,16 @@
-const assertEqual = require('../assertEqual')
+//const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 const head = require('../head')
 
-assertEqual(head(["Lighthouse Labs", "Bootcamp"]), "Lighthouse Labs");
-assertEqual(head([1, 2]), 1);
-assertEqual(head([1, 2, 3]));
+//assertEqual(head(["Lighthouse Labs", "Bootcamp"]), "Lighthouse Labs");
+//assertEqual(head([1, 2]), 1);
+//assertEqual(head([1, 2, 3]));
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", function () {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
