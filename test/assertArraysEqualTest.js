7fdba157865939fc36a9147d68af24b125bf34df
Kiup Kim <kukim96@gmail.com>
2021-04-05 14:39:27 -0600

    refactored

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,4 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual(["I", "Love", "Coding"], ["I", "Love", "Coding"]);
+assertArraysEqual(["I", "Love", "Soccer"], ["I", "Love", "Baseball"]);
