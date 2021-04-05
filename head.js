e57ec9e760f58797419f65b9697c074c837a8da1
Kiup Kim <kukim96@gmail.com>
2021-04-05 14:09:44 -0600

    seperated testcode

--- head.js
+++ head.js
@@ -4,6 +4,4 @@ const head = function(assertEqual) {
   return assertEqual[0];
 };
 
-assertEqual(head(["Lighthouse Labs", "Bootcamp"]), "Lighthouse Labs");
-assertEqual(head([1, 2]), 1);
-assertEqual([1, 2, 3]);
+module.exports = head;
